4d9ca6db893c23656c391d38da05163ff26010bc
Dongwoo Yu <dongwoozzik@naver.com>
2022-01-06 21:36:25 +0900

    2022-01-06 THREE
    THREEJS 연습
    Points와 Line에 대한 재질인 PointsMaterial과 LineBasicMaterial 그리고 LineDashedMaterial

---
+++ three/basic/04_material.js
@@ -0,0 +1,135 @@
+import * as THREE from '../build/three.module.js';
+import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
+
+class App {
+    constructor() {
+        const divContainer = document.querySelector("#webgl-container");
+        this._divContainer = divContainer;
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        divContainer.appendChild(renderer.domElement);
+
+        this._renderer = renderer;
+
+        const scene = new THREE.Scene();
+        this._scene = scene;
+
+        this._setupCamera();
+        this._setupLight();
+        this._setupModel();
+        this._setupControls();
+
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    _setupControls() {
+        new OrbitControls(this._camera, this._divContainer);
+    }
+
+//  Material 정육면체 안에 랜덤위치에 생성되는 구의 집합체 생성
+// _setupModel() {
+//     const vertices = [];
+//     for(let i =0; i <10000; i++) {
+//         const x = THREE.Math.randFloatSpread(5);
+//         const y = THREE.Math.randFloatSpread(5);
+//         const z = THREE.Math.randFloatSpread(5);
+        
+//         vertices.push(x,y,z);
+//     }
+
+//     const geometry = new THREE.BufferGeometry();
+//     geometry.setAttribute(
+//         "position",
+//         new THREE.Float32BufferAttribute(vertices, 3)
+//     );
+
+
+//     const sprite = new THREE.TextureLoader().load(
+//         "../examples/textures/sprites/disc.png"
+//     );
+//     const material = new THREE.PointsMaterial({
+//         map: sprite,
+//         color: "#00ffff",
+//         size: 0.1,
+//         sizeAttenuation: true
+//     });
+
+//     const points = new THREE.Points(geometry, material);
+//     this._scene.add(points);
+// };
+// =============================================================
+
+// vertices의 구성 좌표대로 라인으로 연결돼 생성
+//  Line, Linesegments , LineLoop 등이 있음
+    _setupModel() {
+        const vertices = [
+            -1, 1, 0,
+            1, 1, 0,
+            -1 ,-1 ,0,
+            1, -1, 0
+        ];
+
+        const geometry = new THREE.BufferGeometry();
+        geometry.setAttribute("position",
+        new THREE.Float32BufferAttribute(vertices, 3));
+
+        const material = new THREE.LineDashedMaterial({
+            color: 0xffff00,
+            dashSize: 0.2,
+            gapSize: 0.1,
+            scale: 2
+        });
+
+        const line = new THREE.LineLoop(geometry, material);
+        this._scene.add(line);
+    }
+
+    _setupCamera() {
+        const camera = new THREE.PerspectiveCamera(
+            75, 
+            window.innerWidth / window.innerHeight, 
+            0.1, 
+            100
+        );
+
+        camera.position.z = 15;
+        this._camera = camera;
+    }
+
+    _setupLight() {
+        const color = 0xffffff;
+        const intensity = 1;
+        const light = new THREE.HemisphereLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        this._scene.add(light);
+    }
+
+    update(time) {
+        time *= 0.001; // second unit
+    }
+
+    render(time) {
+        this._renderer.render(this._scene, this._camera);   
+        this.update(time);
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    resize() {
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width / height;
+        this._camera.updateProjectionMatrix();
+        
+        this._renderer.setSize(width, height);
+    }
+}
+
+window.onload = function () {
+    new App();
+}
