ea1f0998cde55da00f9e3cefd00952501ba20f77
Dongwoo Yu <dongwoozzik@naver.com>
2022-01-12 23:01:16 +0900

    2022-01-12
    MeshBasicMaterial, MeshLambertMaterial, MeshPhongMaterial, MeshStandardMaterial, MeshPhysicalMaterial

--- three/basic/04_material.js
+++ three/basic/04_material.js
@@ -87,19 +87,66 @@ class App {
     //     const line = new THREE.LineLoop(geometry, material);
     //     this._scene.add(line);
     // }
-    _setupModel() {
-        const material = new THREE.MeshBasicMaterial({
-            visible : true,
-            transparent: false,
-            opcity: 1,
-            depthTest: true,
-            depthWrite: true,
-            side: THREE.FrontSide,
-            
-            color: 0xffff00,
-            wireframe: false
-        });
+// =================================================================
 
+    // MeshLambertMaterial 매쉬를 구성하는 정점에서 광원의 영향을 계산하는 물질
+
+    // _setupModel() {
+    //     const material = new THREE.MeshLambertMaterial({
+    //         visible : true, // 랜더링시 매쉬가 보일지 안 보일지 지정
+    //         transparent: true, // 재질의 불투명도 사용여부
+    //         opacity: 0.9, // 재질의 불투명도 값 0투명 1불투명
+    //         depthTest: true, // depthBuffer는 깊이 버퍼이고 z버퍼 , z버퍼는 3차원 객체를 카메라를 통한 좌표를 변환시켜 화면상에
+    //         // 랜더링 될때 3차원 객체를 구성하는 각 픽셀에 대한 z값 좌표값을 0~1로 정규화, 이 정규화된 z값이 저장된 버퍼가 z버퍼
+    //         // 이 값이 작을수록 카메라에서 가까운 3차원 객체의 픽셀임 , 카메라로부터 거리에 z 버퍼값이 비례 , z버퍼값이 작을수록 어두운 색상
+    //         depthWrite: true,
+    //         side: THREE.DoubleSide, // 매시를 구성하는 면에 대해서 앞,뒤 렌더링 여부 Front,Back,Double(Side)
+            
+    //         color: '#d25383', // 재질의 색상
+    //         emissive: 0x555500, // 다른광원의 영향을 받지않는 재질 자체에서 방출하는 색상 기본값 검정
+    //         wireframe: true // 매시를 선으로 표현할것인지
+    //     });
+
+// =================================================================
+
+// MeshPhongMaterial
+//      매쉬가 랜더링되는 픽셀단위를 광원의 영향을 계산하는 재질
+// _setupModel() {
+//     const material = new THREE.MeshPhongMaterial({
+//         color: 0xff0000,
+//         emissive: 0x00000,
+//         specular: 0xffff00, // 광원에 의해서 반사되는 색상 기본값 연한 회색
+//         shininess: 10,
+//         flatShading: true, // PBR재질(Physically Based Rendering) MeshStandardMaterial, MeshPhysicalMaterial
+//         wireframe: false
+//     });
+
+// =================================================================
+
+    // _setupModel() {
+    //     const material = new THREE.MeshStandardMaterial({
+    //         color: 0xff0000,
+    //         emissive: 0x00000,
+    //         roughness: 0.25, // 표면의 거칠기
+    //         metalness: 0.7, // 0~1 금속성
+
+    //         flatShading: false, // PBR재질(Physically Based Rendering) MeshStandardMaterial, MeshPhysicalMaterial
+    //         wireframe: false
+    //     });
+
+// =================================================================
+
+_setupModel() {
+    const material = new THREE.MeshPhysicalMaterial({
+        color: 0xff0000,
+        emissive: 0x00000,
+        roughness: 0.25,
+        metalness: 0.5,
+        clearcoat: 0,
+        clearcoatRoughness: 0,
+        wireframe: false,
+        flatshading: false
+    });
         const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
         box.position.set(-1, 0, 0);
         this._scene.add(box);
